6add69f6c2431ef371e17443186c47c389983718
Robin <delarbre.robin@gmail.com>
2026-04-09 23:47:16 +0200

    Bug fixed passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,14 +1,11 @@
 import sys
 import os
 
-# Indique à Passenger où se trouve ton projet
-CHEMIN = os.path.dirname(__file__)
+CHEMIN = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, CHEMIN)
 
-# Charge les variables d'environnement depuis .env
 from dotenv import load_dotenv
 load_dotenv(os.path.join(CHEMIN, ".env"))
 
-# Importe et expose l'app Flask sous le nom 'application'
 from app import create_app
 application = create_app("production")
